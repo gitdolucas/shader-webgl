a48be4fd7afdbba533525f964cc6bb009bf39e6a
Lucas Lima <emailsparalucas@gmail.com>
2023-04-01 00:48:20 -0300

    cool shader 2

--- src/components/fragmentShader.ts
+++ src/components/fragmentShader.ts
@@ -7,8 +7,16 @@ void main() {
     dist = distance(vPosition, vHover);
     vec3 color = vec3(dist);
     vec3 gray = vec3(0.21f, 0.21f, 0.21f);
-    if(dist < 10.0 ){
-      gl_FragColor = vec4( normalize(vPosition * sin(dist * 3.0) + gray) * 15.0  , dist * 80.0); // set the color to gray (default is red)
+    if(dist < 4.0 ){
+      gl_FragColor = vec4(
+        normalize(
+            0.2  
+            + dist * 0.15 
+            * vPosition 
+            * sin(dist * 1.0) 
+            + cos(gray * -10.0) 
+            * sin(vPosition.x * 8.0)
+        ) * 10.0  , 1.0); // set the color to gray (default is red)
     } else {
       gl_FragColor = vec4(gray, 1.0f); // set the color to gray (default is red)
     }
